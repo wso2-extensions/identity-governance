f13419157ad6296d008e184ae388ec4f8160c52c
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2020-07-03 21:59:11 +0530

    Modify connector field names and descriptions and fix minor issues in password history component

--- components/org.wso2.carbon.identity.password.history/src/test/java/org/wso2/carbon/identity/password/history/handler/PasswordHistoryValidationHandlerTest.java
+++ components/org.wso2.carbon.identity.password.history/src/test/java/org/wso2/carbon/identity/password/history/handler/PasswordHistoryValidationHandlerTest.java
@@ -18,10 +18,12 @@ package org.wso2.carbon.identity.password.history.handler;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.password.history.constants.PasswordHistoryConstants;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertFalse;
@@ -32,99 +34,69 @@ public class PasswordHistoryValidationHandlerTest {
 
     @BeforeTest
     public void setUp() throws IllegalAccessException, InstantiationException {
+
         passwordHistoryValidationHandler = new PasswordHistoryValidationHandler();
     }
 
     @Test
     public void testGetName() throws Exception {
-        assertEquals(passwordHistoryValidationHandler.getName(),
-                "passwordHistory", "Valid getName().");
-        assertNotEquals(passwordHistoryValidationHandler.getName(),
-                "passwordkshfy", "Invalid getName().");
-        assertFalse(Boolean.parseBoolean(passwordHistoryValidationHandler.getName()),
-                "Invalid getName().");
+
+        assertEquals(passwordHistoryValidationHandler.getName(), "passwordHistory");
     }
 
     @Test
     public void testGetFriendlyName() throws Exception {
-       assertEquals(passwordHistoryValidationHandler.getFriendlyName(),
-                "Password History", "Valid getFriendlyName().");
-       assertNotEquals(passwordHistoryValidationHandler.getFriendlyName(),
-                "passwordHistory", "Invalid getFriendlyName().");
+
+        assertEquals(passwordHistoryValidationHandler.getFriendlyName(), "Password History");
     }
 
     @Test
     public void testGetCategory() throws Exception {
-        assertEquals(passwordHistoryValidationHandler.getCategory(),
-                "Password Policies", "Valid getCategory().");
-        assertNotEquals(passwordHistoryValidationHandler.getCategory(),
-                "PasswordPolicies", "Invalid getCategory().");
-    }
 
+        assertEquals(passwordHistoryValidationHandler.getCategory(), "User Passwords");
+    }
 
     @Test
     public void testGetSubCategory() throws Exception {
-        assertEquals(passwordHistoryValidationHandler.getSubCategory(),
-                "DEFAULT", "Valid getSubCategory().");
-        assertNotEquals(passwordHistoryValidationHandler.getSubCategory(),
-                "PasswordPolicies", "Invalid getSubCategory().");
+
+        assertEquals(passwordHistoryValidationHandler.getSubCategory(), "DEFAULT");
     }
 
     @Test
     public void testGetOrder() throws Exception {
-        assertEquals(passwordHistoryValidationHandler.getOrder(),
-                0, "Valid getOrder().");
-        assertNotEquals(passwordHistoryValidationHandler.getOrder(),
-                2, "Invalid getOrder().");
+
+        assertEquals(passwordHistoryValidationHandler.getOrder(), 0);
     }
 
     @Test
     public void testGetPropertyNameMapping() throws Exception {
-        Map<String, String> expectedMappingTrue = new HashMap<>();
-        expectedMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_ENABLE, "Enable Password History Feature");
-        expectedMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_COUNT, "Password History validation count");
-        assertEquals(passwordHistoryValidationHandler.getPropertyNameMapping(), expectedMappingTrue,
-                "Valid getPropertyNameMapping() .");
 
-        Map<String, String> expectedMappingFalse = new HashMap<>();
-        expectedMappingFalse.put(PasswordHistoryConstants.PW_HISTORY_ENABLE, "Enable Password History");
-        expectedMappingFalse.put(PasswordHistoryConstants.PW_HISTORY_COUNT, "Password History validation");
-        assertNotEquals(passwordHistoryValidationHandler.getPropertyNameMapping(), expectedMappingFalse,
-                "Invalid getPropertyNameMapping() .");
+        Map<String, String> expectedMapping = new HashMap<>();
+        expectedMapping.put(PasswordHistoryConstants.PW_HISTORY_ENABLE, "Validate password history");
+        expectedMapping.put(PasswordHistoryConstants.PW_HISTORY_COUNT, "Password history validation count");
+        assertEquals(passwordHistoryValidationHandler.getPropertyNameMapping(), expectedMapping,
+                "Property names has been changed.");
     }
 
     @Test
     public void testGetPropertyDescriptionMapping() throws Exception {
-        Map<String, String> expecteddescriptionMappingTrue = new HashMap<>();
-        expecteddescriptionMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_ENABLE,
-                "Enable to disallow previously used passwords");
-        expecteddescriptionMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_COUNT,
-                "Restrict reusing last x number of password during password update");
-        assertEquals(passwordHistoryValidationHandler.getPropertyDescriptionMapping(),
-                expecteddescriptionMappingTrue, "Valid getPropertyDescriptionMapping().");
 
-        Map<String, String> expecteddescriptionMappingFalse = new HashMap<>();
-        expecteddescriptionMappingFalse.put(PasswordHistoryConstants.PW_HISTORY_ENABLE,
-                "Enable to allow previously used passwords");
-        expecteddescriptionMappingFalse.put(PasswordHistoryConstants.PW_HISTORY_COUNT,
-                "Restrict reusing last number of password during password update");
-        assertNotEquals(passwordHistoryValidationHandler.getPropertyDescriptionMapping(),
-                expecteddescriptionMappingFalse, "Invalid getPropertyDescriptionMapping().");
+        Map<String, String> expectedDescriptionMappingTrue = new HashMap<>();
+        expectedDescriptionMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_ENABLE, "User will not be allowed to " +
+                "use previously used passwords.");
+        expectedDescriptionMappingTrue.put(PasswordHistoryConstants.PW_HISTORY_COUNT, "Restrict reusing last n " +
+                "number of passwords during password update.");
+        assertEquals(passwordHistoryValidationHandler.getPropertyDescriptionMapping(),
+                expectedDescriptionMappingTrue, "Property descriptions has been changed.");
     }
 
     @Test
     public void testGetPropertyNames() throws Exception {
-        List<String> expectedpropertiesTrue = new ArrayList<>();
-        expectedpropertiesTrue.add(PasswordHistoryConstants.PW_HISTORY_ENABLE);
-        expectedpropertiesTrue.add(PasswordHistoryConstants.PW_HISTORY_COUNT);
-        assertEquals(passwordHistoryValidationHandler.getPropertyNames(),
-                expectedpropertiesTrue.toArray(new String[expectedpropertiesTrue.size()]),
-                "Valid getPropertyDescriptionMapping().");
 
-        List<String> expectedpropertiesFalse = new ArrayList<>();
-        expectedpropertiesFalse.add(PasswordHistoryConstants.PW_HISTORY_ENABLE);
-        assertNotEquals(passwordHistoryValidationHandler.getPropertyNames(),
-                expectedpropertiesFalse.toArray(new String[expectedpropertiesFalse.size()]),
-                "Valid getPropertyDescriptionMapping().");
+        List<String> expectedPropertiesTrue = new ArrayList<>();
+        expectedPropertiesTrue.add(PasswordHistoryConstants.PW_HISTORY_ENABLE);
+        expectedPropertiesTrue.add(PasswordHistoryConstants.PW_HISTORY_COUNT);
+        assertEquals(passwordHistoryValidationHandler.getPropertyNames(),
+                expectedPropertiesTrue.toArray(new String[0]), "getPropertyNames() has been changed.");
     }
 }
