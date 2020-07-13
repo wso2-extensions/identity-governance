e2278c1162154f3d237e2d0dca4f5eb22186ecc9
Pushpalanka Jayawardhana <lanka@wso2.com>
2020-07-13 15:12:31 +0530

    Add new email template for resending email confirmation for lite user. (#404)

--- components/org.wso2.carbon.identity.user.endpoint/src/main/java/org/wso2/carbon/identity/user/endpoint/impl/ResendCodeApiServiceImpl.java
+++ components/org.wso2.carbon.identity.user.endpoint/src/main/java/org/wso2/carbon/identity/user/endpoint/impl/ResendCodeApiServiceImpl.java
@@ -151,7 +151,7 @@ public class ResendCodeApiServiceImpl extends ResendCodeApiService {
                 RecoverySteps.CONFIRM_LITE_SIGN_UP.equals(userRecoveryData.getRecoveryStep())) {
             notificationResponseBean = setNotificationResponseBean(resendConfirmationManager,
                     RecoveryScenarios.LITE_SIGN_UP.toString(), RecoverySteps.CONFIRM_LITE_SIGN_UP.toString(),
-                    IdentityRecoveryConstants.NOTIFICATION_TYPE_LITE_USER_EMAIL_CONFIRM, resendCodeRequestDTO);
+                    IdentityRecoveryConstants.NOTIFICATION_TYPE_RESEND_LITE_USER_EMAIL_CONFIRM, resendCodeRequestDTO);
         }
 
         return notificationResponseBean;
