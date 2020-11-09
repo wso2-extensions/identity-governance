c8046bcc18b4096d9ecdee85be6de8568f710f48
Geesa-Vihara <geesavihara97@gmail.com>
2020-11-09 10:20:08 +0530

    Password recovery no email fix

--- components/org.wso2.carbon.identity.recovery.endpoint/src/main/java/org/wso2/carbon/identity/recovery/endpoint/impl/RecoverPasswordApiServiceImpl.java
+++ components/org.wso2.carbon.identity.recovery.endpoint/src/main/java/org/wso2/carbon/identity/recovery/endpoint/impl/RecoverPasswordApiServiceImpl.java
@@ -81,6 +81,12 @@ public class RecoverPasswordApiServiceImpl extends RecoverPasswordApiService {
         } catch (IdentityRecoveryException e) {
             RecoveryUtil.handleInternalServerError(Constants.SERVER_ERROR, e.getErrorCode(), LOG, e);
         } catch (Throwable throwable) {
+            if (throwable != null && StringUtils.equals(Constants.ERROR_MESSAGE_EMAIL_NOT_FOUND,
+                    throwable.getMessage())) {
+                String msg = "You have not set the users email address. Please set the email address and reset again.";
+                LOG.error(msg);
+                RecoveryUtil.handleBadRequest(msg, Constants.ERROR_CODE_EMAIL_NOT_FOUND);
+            }
             RecoveryUtil.handleInternalServerError(Constants.SERVER_ERROR, IdentityRecoveryConstants
                     .ErrorMessages.ERROR_CODE_UNEXPECTED.getCode(), LOG, throwable);
         }
