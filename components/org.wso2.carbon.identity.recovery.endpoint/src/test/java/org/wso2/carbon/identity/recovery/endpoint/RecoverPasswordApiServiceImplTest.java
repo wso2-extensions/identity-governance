60c1506df29c2f2621352ce2f53063a51d0109e3
chathurangap688 <bmcpthilakawansha@gmail.com>
2021-03-16 10:06:44 +0530

    Add multi attribute identifier to password recovery
    (cherry picked from commit 96a8b1598b28b107a948952b8f8e1262a4b07ae0)

--- components/org.wso2.carbon.identity.recovery.endpoint/src/test/java/org/wso2/carbon/identity/recovery/endpoint/RecoverPasswordApiServiceImplTest.java
+++ components/org.wso2.carbon.identity.recovery.endpoint/src/test/java/org/wso2/carbon/identity/recovery/endpoint/RecoverPasswordApiServiceImplTest.java
@@ -24,8 +24,10 @@ import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.IObjectFactory;
 import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
+import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
 import org.wso2.carbon.identity.application.common.model.User;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
+import org.wso2.carbon.identity.multi.attribute.login.mgt.ResolvedUserResult;
 import org.wso2.carbon.identity.recovery.IdentityRecoveryException;
 import org.wso2.carbon.identity.recovery.bean.NotificationResponseBean;
 import org.wso2.carbon.identity.recovery.endpoint.Utils.RecoveryUtil;
@@ -49,7 +51,7 @@ import static org.testng.Assert.assertEquals;
 /**
  * This class covers unit tests for RecoverPasswordApiServiceImpl.java
  */
-@PrepareForTest({RecoveryUtil.class, IdentityTenantUtil.class})
+@PrepareForTest({RecoveryUtil.class, IdentityTenantUtil.class, FrameworkUtils.class})
 public class RecoverPasswordApiServiceImplTest extends PowerMockTestCase {
     
     @Mock
@@ -68,8 +70,11 @@ public class RecoverPasswordApiServiceImplTest extends PowerMockTestCase {
         mockStatic(RecoveryUtil.class);
         when(IdentityTenantUtil.getTenantId(anyString())).thenReturn(-1234);
         when(RecoveryUtil.getNotificationBasedPwdRecoveryManager()).thenReturn(notificationPasswordRecoveryManager);
+        mockStatic(FrameworkUtils.class);
+        ResolvedUserResult resolvedUserResult = new ResolvedUserResult(ResolvedUserResult.UserResolvedStatus.FAIL);
         when(notificationPasswordRecoveryManager.sendRecoveryNotification(any(User.class), anyString(), anyBoolean(),
                 any(Property[].class))).thenReturn(notificationResponseBean);
+        when(FrameworkUtils.processMultiAttributeLoginIdentification(anyString(), anyString())).thenReturn(resolvedUserResult);
         assertEquals(recoverPasswordApiService.recoverPasswordPost(buildRecoveryInitiatingRequestDTO(), "", true).
                 getStatus(), 202);
     }
