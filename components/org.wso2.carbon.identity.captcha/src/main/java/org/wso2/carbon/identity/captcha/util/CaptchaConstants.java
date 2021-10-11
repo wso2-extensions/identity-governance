3b509e79ddcb1e3c6c4dc37798979762a027fd3c
Thisara-Welmilla <thisara@wso2.com>
2021-10-11 17:14:20 +0530

    refactored code

--- components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/util/CaptchaConstants.java
+++ components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/util/CaptchaConstants.java
@@ -41,7 +41,7 @@ public class CaptchaConstants {
 
     public static final String RE_CAPTCHA_SECRET_KEY = "recaptcha.secret.key";
 
-    public static final String RE_CAPTCHA_PARAMETERS_IN_URL_ENABLED = "recaptcha.ParametersInURL.enabled";
+    public static final String RE_CAPTCHA_PARAMETERS_IN_URL_ENABLED = "recaptcha.parametersInURL.enabled";
 
     public static final String FAIL_LOGIN_ATTEMPT_VALIDATOR_ENABLED = "failLoginAttemptValidator.enable";
 
