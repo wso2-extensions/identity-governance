b6ff412c80459b6b52954b9265a5a8c2f5092006
Thisara-Welmilla <thisara@wso2.com>
2023-03-15 08:17:21 +0530

    Refactored code

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/signup/UserSelfRegistrationManager.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/signup/UserSelfRegistrationManager.java
@@ -1,18 +1,17 @@
 /*
+ * Copyright (c) 2016, WSO2 LLC. (https://www.wso2.com) All Rights Reserved.
  *
- * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -1513,8 +1512,8 @@ public class UserSelfRegistrationManager {
 
     /** This method is to validate username format against the regex from the userstore.
      *
-     * @param userName      Username that needs to be validated
-     * @param realmConfig   Realm Configuration
+     * @param userName      Username that needs to be validated.
+     * @param realmConfig   Realm Configuration.
      * @return whether username values satisfy the regex.
      */
     private boolean validateAgainstRegex(String userName, RealmConfiguration realmConfig) {
@@ -1548,6 +1547,7 @@ public class UserSelfRegistrationManager {
     }
 
     /** This method is to validate username format against the username validation configured with Input Validation.
+     *
      * @param configuration     Validation configuration for the username field.
      * @param validators        Validators.
      * @param field             Field name that need to be validated.
