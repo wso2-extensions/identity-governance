1845c54adca23e75fe9e7ed38230b4da0b42ced4
Inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2023-04-17 09:14:01 +0530

    Add license.

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/connector/OrganizationConfigImp.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/connector/OrganizationConfigImp.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.identity.recovery.connector;
 
 import org.apache.commons.lang.StringUtils;
@@ -16,6 +33,9 @@ import java.util.Properties;
 
 import static org.wso2.carbon.identity.governance.IdentityGovernanceUtil.getPropertyObject;
 
+/**
+ * Class which contains Sub organization mgt configs.
+ */
 public class OrganizationConfigImp implements IdentityConnectorConfig {
 
 
@@ -24,8 +44,7 @@ public class OrganizationConfigImp implements IdentityConnectorConfig {
     public static final String ENABLE_SELF_SERVICE = "Organization.SelfService.Enable";
     public static final String DEFAULT_SUB_ORGANIZATION_TIER = "Organization.SelfService.DefaultSubOrgTier";
 
-    private static final String connectorName = "organization-self-service";
-
+    private static final String CONNECTOR_NAME = "organization-self-service";
     private static final String CATEGORY = "User Onboarding";
     private static final String FRIENDLY_NAME = "Sub Organization Self Service";
 
@@ -33,7 +52,7 @@ public class OrganizationConfigImp implements IdentityConnectorConfig {
     @Override
     public String getName() {
 
-        return connectorName;
+        return CONNECTOR_NAME;
     }
 
     @Override
@@ -94,7 +113,6 @@ public class OrganizationConfigImp implements IdentityConnectorConfig {
         properties.add(ENABLE_ADMIN_EMAIL_VERIFICATION);
         properties.add(ONBOARD_ADMIN_TO_SUB_ORGANIZATION);
         properties.add(DEFAULT_SUB_ORGANIZATION_TIER);
-
         return properties.toArray(new String[0]);
     }
 
