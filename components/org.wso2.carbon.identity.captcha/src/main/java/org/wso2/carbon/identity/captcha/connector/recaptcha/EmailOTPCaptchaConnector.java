6184a1c0f89037254e56998a77e3601ab5c36fa1
Amanda Ariyaratne <amandaa@wso2.com>
2023-05-22 11:53:19 +0530

    Use SSO Login Recaptcha config to Email OTP connector

--- components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/connector/recaptcha/EmailOTPCaptchaConnector.java
+++ components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/connector/recaptcha/EmailOTPCaptchaConnector.java
@@ -1,20 +1,26 @@
 package org.wso2.carbon.identity.captcha.connector.recaptcha;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.util.FrameworkUtils;
+import org.wso2.carbon.identity.application.common.model.Property;
 import org.wso2.carbon.identity.captcha.connector.CaptchaPostValidationResponse;
 import org.wso2.carbon.identity.captcha.connector.CaptchaPreValidationResponse;
 import org.wso2.carbon.identity.captcha.exception.CaptchaException;
 import org.wso2.carbon.identity.captcha.internal.CaptchaDataHolder;
+import org.wso2.carbon.identity.captcha.util.CaptchaConstants;
 import org.wso2.carbon.identity.captcha.util.CaptchaUtil;
+import org.wso2.carbon.identity.governance.IdentityGovernanceException;
 import org.wso2.carbon.identity.governance.IdentityGovernanceService;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+
+import static org.wso2.carbon.identity.captcha.util.CaptchaConstants.SSO_LOGIN_RECAPTCHA_CONNECTOR_NAME;
 
 /**
  * Recaptcha Connector for Email OTP.
@@ -23,7 +29,6 @@ public class EmailOTPCaptchaConnector extends AbstractReCaptchaConnector {
 
     private static final Log log = LogFactory.getLog(EmailOTPCaptchaConnector.class);
     private static final String SECURED_DESTINATIONS = "/commonauth";
-    private static final String EMAIL_OTP_RECAPTCHA_ENABLE = "EmailOTP.RecaptchaEnable";
     public static final String EMAIL_OTP_AUTHENTICATOR_NAME = "email-otp-authenticator";
     public static final String IS_REDIRECT_TO_EMAIL_OTP = "isRedirectToEmailOTP";
     public static final String RESEND_CODE = "resendCode";
@@ -86,8 +91,45 @@ public class EmailOTPCaptchaConnector extends AbstractReCaptchaConnector {
         if (CaptchaDataHolder.getInstance().isForcefullyEnabledRecaptchaForAllTenants()) {
             return true;
         }
-        return CaptchaDataHolder.getInstance().isReCaptchaEnabled()
-                && CaptchaUtil.isRecaptchaEnabledForConnector(this.identityGovernanceService, request,
-                EMAIL_OTP_RECAPTCHA_ENABLE);
+
+        String sessionDataKey = request.getParameter(FrameworkUtils.SESSION_DATA_KEY);
+        if (sessionDataKey == null) {
+            return false;
+        }
+        AuthenticationContext context = FrameworkUtils.getAuthenticationContextFromCache(sessionDataKey);
+        if (context == null) {
+            return false;
+        }
+
+        String tenantDomain = context.getTenantDomain();
+        if (StringUtils.isBlank(tenantDomain)) {
+            return false;
+        }
+
+        Property[] connectorConfigs;
+        try {
+            connectorConfigs = identityGovernanceService.getConfiguration(new String[]{
+                SSO_LOGIN_RECAPTCHA_CONNECTOR_NAME + CaptchaConstants.ReCaptchaConnectorPropertySuffixes.ENABLE_ALWAYS,
+                SSO_LOGIN_RECAPTCHA_CONNECTOR_NAME + CaptchaConstants.ReCaptchaConnectorPropertySuffixes.ENABLE},
+                tenantDomain);
+        } catch (IdentityGovernanceException e) {
+            // Can happen due to invalid user/ invalid tenant/ invalid configuration.
+            if (log.isDebugEnabled()) {
+                log.debug("Unable to load connector configuration.", e);
+            }
+            return false;
+        }
+
+        if (ArrayUtils.isEmpty(connectorConfigs) || connectorConfigs.length != 2 ||
+                !(Boolean.parseBoolean(connectorConfigs[0].getValue()) ||
+                        Boolean.parseBoolean(connectorConfigs[1].getValue()))) {
+            return false;
+        }
+
+        if (!CaptchaDataHolder.getInstance().isReCaptchaEnabled()) {
+            return false;
+        }
+
+        return true;
     }
 }
