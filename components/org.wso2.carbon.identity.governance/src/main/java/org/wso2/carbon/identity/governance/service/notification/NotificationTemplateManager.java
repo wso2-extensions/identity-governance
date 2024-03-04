58587abf4c12a0b457e48a41093beb5709daa915
Avarjana <acpasavarjana@gmail.com>
2024-03-04 13:54:00 +0530

    Address review comments

--- components/org.wso2.carbon.identity.governance/src/main/java/org/wso2/carbon/identity/governance/service/notification/NotificationTemplateManager.java
+++ components/org.wso2.carbon.identity.governance/src/main/java/org/wso2/carbon/identity/governance/service/notification/NotificationTemplateManager.java
@@ -45,13 +45,13 @@ public interface NotificationTemplateManager {
     /**
      * Return the notification template from the tenant registry which matches the given channel and template name.
      *
-     * @param notificationChannel Notification Channel Name (Eg: SMS or EMAIL)
-     * @param templateType        Display name of the template
-     * @param locale              Locale
-     * @param tenantDomain        Tenant Domain
-     * @param applicationUuid     Application UUID
-     * @return Return {@link org.wso2.carbon.identity.governance.model.NotificationTemplate} object
-     * @throws NotificationTemplateManagerException If an error occurred while getting the notification template
+     * @param notificationChannel Notification Channel Name (Eg: SMS or EMAIL).
+     * @param templateType        Display name of the template.
+     * @param locale              Locale.
+     * @param tenantDomain        Tenant Domain.
+     * @param applicationUuid     Application UUID.
+     * @return Return {@link org.wso2.carbon.identity.governance.model.NotificationTemplate} object.
+     * @throws NotificationTemplateManagerException If an error occurred while getting the notification template.
      */
     default NotificationTemplate getNotificationTemplate(String notificationChannel, String templateType, String locale,
                                                          String tenantDomain, String applicationUuid)
@@ -76,9 +76,9 @@ public interface NotificationTemplateManager {
     /**
      * Add the notification template to the registry.
      *
-     * @param notificationTemplate  Notification template
-     * @param tenantDomain          Tenant domain
-     * @param applicationUuid       Application UUID
+     * @param notificationTemplate  Notification template.
+     * @param tenantDomain          Tenant domain.
+     * @param applicationUuid       Application UUID.
      * @throws NotificationTemplateManagerException If an error occurred while adding the notification template.
      */
     default void addNotificationTemplate(NotificationTemplate notificationTemplate, String tenantDomain,
@@ -126,11 +126,11 @@ public interface NotificationTemplateManager {
     /**
      * Add a new notification template to the registry to the corresponding notification channel root directory.
      *
-     * @param displayName         Notification template display name
-     * @param notificationChannel Notification channel
-     * @param tenantDomain        Tenant domain
-     * @param applicationUuid     Application UUID
-     * @throws NotificationTemplateManagerException If an error occurred while adding the template to the registry
+     * @param displayName         Notification template display name.
+     * @param notificationChannel Notification channel (Eg: SMS, EMAIL).
+     * @param tenantDomain        Tenant domain.
+     * @param applicationUuid     Application UUID.
+     * @throws NotificationTemplateManagerException If an error occurred while adding the template to the registry.
      */
     default void addNotificationTemplateType(String displayName, String notificationChannel,
                                              String tenantDomain, String applicationUuid)
