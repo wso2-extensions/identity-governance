34fcb7528b207370d875ae1ca004ac2ac531e3e5
rushannanayakkara <rushannana@gmail.com>
2024-05-21 11:08:48 +0530

    Limit API access to notification channels based on governance configs.

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/internal/service/impl/password/PasswordRecoveryManagerImpl.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/internal/service/impl/password/PasswordRecoveryManagerImpl.java
@@ -36,6 +36,7 @@ import org.wso2.carbon.identity.recovery.RecoveryScenarios;
 import org.wso2.carbon.identity.recovery.RecoverySteps;
 import org.wso2.carbon.identity.recovery.bean.NotificationResponseBean;
 import org.wso2.carbon.identity.recovery.confirmation.ResendConfirmationManager;
+import org.wso2.carbon.identity.recovery.dto.NotificationChannelDTO;
 import org.wso2.carbon.identity.recovery.dto.PasswordRecoverDTO;
 import org.wso2.carbon.identity.recovery.dto.PasswordResetCodeDTO;
 import org.wso2.carbon.identity.recovery.dto.RecoveryChannelInfoDTO;
@@ -57,6 +58,7 @@ import org.wso2.carbon.identity.user.functionality.mgt.model.FunctionalityLockSt
 
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Pattern;
@@ -103,6 +105,13 @@ public class PasswordRecoveryManagerImpl implements PasswordRecoveryManager {
         RecoveryChannelInfoDTO recoveryChannelInfoDTO = userAccountRecoveryManager
                 .retrieveUserRecoveryInformation(claims, tenantDomain, RecoveryScenarios.NOTIFICATION_BASED_PW_RECOVERY,
                         properties);
+        List<NotificationChannelDTO> list = new ArrayList<>();
+        for (NotificationChannelDTO notificationChannelDTO : recoveryChannelInfoDTO.getNotificationChannelDTOs()) {
+            if (isRecoveryChannelEnabled(notificationChannelDTO.getType(), tenantDomain)) {
+                list.add(notificationChannelDTO);
+            }
+        }
+        recoveryChannelInfoDTO.setNotificationChannelDTOs(list.toArray(new NotificationChannelDTO[0]));
         String username = recoveryChannelInfoDTO.getUsername();
         String recoveryFlowId = recoveryChannelInfoDTO.getRecoveryFlowId();
         recoveryInformationDTO.setUsername(username);
@@ -138,6 +147,12 @@ public class PasswordRecoveryManagerImpl implements PasswordRecoveryManager {
                 .getUserRecoveryData(recoveryCode, RecoverySteps.SEND_RECOVERY_INFORMATION);
         String notificationChannel = extractNotificationChannelDetails(userRecoveryData.getRemainingSetIds(),
                 channelIDCode);
+        if(!isRecoveryChannelEnabled(notificationChannel, tenantDomain)) {
+            throw Utils.handleClientException(
+                    IdentityRecoveryConstants.ErrorMessages.ERROR_CODE_INVALID_CHANNEL_ID.getCode(),
+                    IdentityRecoveryConstants.ErrorMessages.ERROR_CODE_INVALID_CHANNEL_ID.getMessage(),
+                    notificationChannel);
+        }
         // Resolve notify status according to the notification channel of the user.
         boolean manageNotificationsInternally = true;
         if (NotificationChannels.EXTERNAL_CHANNEL.getChannelType().equals(notificationChannel)) {
@@ -890,4 +905,16 @@ public class PasswordRecoveryManagerImpl implements PasswordRecoveryManager {
         }
         return buildPasswordResetCodeDTO(confirmationCode);
     }
+
+    private boolean isRecoveryChannelEnabled(String notificationChannelType, String tenantDomain)
+            throws IdentityRecoveryServerException {
+
+        if (NotificationChannels.EMAIL_CHANNEL.getChannelType().equals(notificationChannelType)) {
+            return isEmailLinkBasedRecoveryEnabled(tenantDomain);
+        } else if (NotificationChannels.SMS_CHANNEL.getChannelType().equals(notificationChannelType)) {
+            return isSMSOTPBasedRecoveryEnabled(tenantDomain);
+        }
+        return false;
+    }
+
 }
