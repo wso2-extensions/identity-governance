70c8f38f2da7490137e9775f2f91f371cf150688
Sachith Manchanayaka <42811248+Manchiz@users.noreply.github.com>
2024-06-27 11:17:42 +0530

    Update components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/IdentityRecoveryConstants.java
    Co-authored-by: Avarjana <acpasavarjana@gmail.com>

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/IdentityRecoveryConstants.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/IdentityRecoveryConstants.java
@@ -829,7 +829,7 @@ public class IdentityRecoveryConstants {
         public static final String RESIDENT_IDP = "Resident IDP";
 
         // This is used for retrieve SP-UUID from the event.
-        //To-DO: onc ethe framework version bump to 7.3.14.x or above use the constant from framework insted of this.
+        // Todo: once the framework version bump to 7.3.14.x or above use the constant from framework instead of this.
         public static final String SERVICE_PROVIDER_UUID = "serviceProviderUUID";
     }
 
