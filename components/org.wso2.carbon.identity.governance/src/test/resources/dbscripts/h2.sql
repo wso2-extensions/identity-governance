f028a0d946ed653f3821d92a07e647103cc631f9
Afra Hussaindeen <47135592+AfraHussaindeen@users.noreply.github.com>
2025-05-29 21:06:28 +0530

    Support ne operation for user's filtering with identity claims (#965)
    * Support ne operation for user's filtering with identity claims
    
    * Bump kernel version
    
    * Add unit tests

--- components/org.wso2.carbon.identity.governance/src/test/resources/dbscripts/h2.sql
+++ components/org.wso2.carbon.identity.governance/src/test/resources/dbscripts/h2.sql
@@ -17,4 +17,5 @@ INSERT INTO IDN_IDENTITY_USER_DATA (TENANT_ID, USER_NAME, DATA_KEY, DATA_VALUE) 
 (3, 'DEFAULT/sampleUser5@xmail.com', 'http://wso2.org/claims/identity/lastLogonTime', '1674950400000'),
 (3, 'DEFAULT/sampleUser1@xmail.com', 'http://wso2.org/claims/identity/accountState', 'DISABLED'),
 (3, 'DEFAULT/sampleUser3@xmail.com', 'http://wso2.org/claims/identity/accountState', 'DISABLED'),
-(3, 'DEFAULT/sampleUser5@xmail.com', 'http://wso2.org/claims/identity/accountState', 'DISABLED');
+(3, 'DEFAULT/sampleUser5@xmail.com', 'http://wso2.org/claims/identity/accountState', 'DISABLED'),
+(3, 'DEFAULT/sampleUser5@xmail.com', 'http://wso2.org/claims/identity/emailVerified', 'true');
