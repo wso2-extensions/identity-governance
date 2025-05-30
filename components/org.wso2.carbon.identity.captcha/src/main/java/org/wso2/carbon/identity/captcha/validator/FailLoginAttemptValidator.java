a32651678f66273533c2042e55da843222506f35
Pasindu Yeshan <pasinduyeshan98@gmail.com>
2025-05-30 16:08:11 +0530

    Fix username issue in fail login attempt

--- components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/validator/FailLoginAttemptValidator.java
+++ components/org.wso2.carbon.identity.captcha/src/main/java/org/wso2/carbon/identity/captcha/validator/FailLoginAttemptValidator.java
@@ -88,7 +88,7 @@ public class FailLoginAttemptValidator extends AbstractIdentityMessageHandler im
 
             if (map.get(FrameworkConstants.AnalyticsAttributes.USER) instanceof User) {
                 User failedUser = (User) map.get(FrameworkConstants.AnalyticsAttributes.USER);
-                String username = failedUser.getUserName();
+                String username = failedUser.toFullQualifiedUsername();
                 if (!StringUtils.isBlank(failedUser.getUserStoreDomain()) &&
                         !IdentityUtil.getPrimaryDomainName().equals(failedUser.getUserStoreDomain())) {
                     username = UserCoreUtil.addDomainToName(username, failedUser.getUserStoreDomain());
