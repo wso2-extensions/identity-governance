67a01c84a6ebc0d7c7efb97b089aa6d899a065e3
ImalshaD <plid475@gmail.com>
2025-09-04 09:39:51 +0530

    Add Attribute Onboard listener.

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/internal/IdentityRecoveryServiceComponent.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/internal/IdentityRecoveryServiceComponent.java
@@ -68,6 +68,7 @@ import org.wso2.carbon.identity.recovery.handler.UserEmailVerificationHandler;
 import org.wso2.carbon.identity.recovery.handler.UserSelfRegistrationHandler;
 import org.wso2.carbon.identity.recovery.internal.service.impl.password.PasswordRecoveryManagerImpl;
 import org.wso2.carbon.identity.recovery.internal.service.impl.username.UsernameRecoveryManagerImpl;
+import org.wso2.carbon.identity.recovery.listener.AttributeOnboardListener;
 import org.wso2.carbon.identity.recovery.listener.InvitedRegistrationCompletionListener;
 import org.wso2.carbon.identity.recovery.listener.SelfRegistrationCompletionListener;
 import org.wso2.carbon.identity.recovery.listener.TenantManagementListener;
@@ -146,6 +147,7 @@ public class IdentityRecoveryServiceComponent {
                                                        null);
             bundleContext.registerService(FlowExecutionListener.class, new InvitedRegistrationCompletionListener(),
                     null);
+            bundleContext.registerService(FlowExecutionListener.class, new AttributeOnboardListener(), null);
             bundleContext.registerService(Executor.class.getName(),
                     new ConfirmationCodeValidationExecutor(), null);
             bundleContext.registerService(Executor.class.getName(),
